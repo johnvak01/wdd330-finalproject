6b721c47e5393169dfa38f8ffd55a8f8baf250ce
johnvak01 <johnvak01@gmail.com>
2026-07-28 19:22:14 -0600

    added scripting for automatic headers and footers

---
+++ src/js/utils.mjs
@@ -0,0 +1,24 @@
+export async function loadTemplate(path) {
+    const response = await fetch(path);
+    const template = await response.text();
+    return template;
+}
+
+export function renderWithTemplate(template, parentElement, data, callback){
+    parentElement.innerHTML = template;
+    if (callback) {  
+      callback(data);
+    }
+  }
+
+export async function loadHeaderFooter() {
+    const headerElement = document.querySelector("header");
+    const headerTemplate = await loadTemplate("partials/header.html");
+
+    const footerElement = document.querySelector("footer");
+    const footerTemplate = await loadTemplate("partials/footer.html");
+
+
+    renderWithTemplate(headerTemplate, headerElement);
+    renderWithTemplate(footerTemplate, footerElement);
+} 
