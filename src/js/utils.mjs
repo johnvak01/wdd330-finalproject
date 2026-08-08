3593da54fd28f15a6a20a860c904763f351509cd
johnvak01 <johnvak01@gmail.com>
2026-08-07 21:21:55 -0600

    added basic code for probing a data point.

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,24 +1,48 @@
 export async function loadTemplate(path) {
-    const response = await fetch(path);
-    const template = await response.text();
-    return template;
+  const response = await fetch(path);
+  const template = await response.text();
+  return template;
 }
 
-export function renderWithTemplate(template, parentElement, data, callback){
-    parentElement.innerHTML = template;
-    if (callback) {  
-      callback(data);
-    }
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
   }
+}
 
 export async function loadHeaderFooter() {
-    const headerElement = document.querySelector("header");
-    const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("header");
+  const headerTemplate = await loadTemplate("../partials/header.html");
 
-    const footerElement = document.querySelector("footer");
-    const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("footer");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
 
 
-    renderWithTemplate(headerTemplate, headerElement);
-    renderWithTemplate(footerTemplate, footerElement);
-} 
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
+
+
+
+export class ExternalService {
+  constructor(url) {
+    this.url = url;
+  }
+  #convertToJson(res) {
+    let result = res.json();
+    console.log(result);
+    if (res.ok) {
+      return result;
+    } else {
+      throw { message: "Bad Response", status: res.status, statusText: result };
+    };
+  }
+  async getData(URLextension = "", queryParams = "") {
+    const response = await fetch(`${this.url + extension + queryParams}`);
+    const data = await this.#convertToJson(response);
+
+    return data.Result
+  }
+  
+}
